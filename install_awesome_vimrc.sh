faa92f879a59aac448d2927c855995237f254624
lihao <lihao@fabu.ai>
2019-06-10 21:15:03 +0800

    update

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 set -e
 
-cd ~/.vim_runtime
+VIMDIR=${HOME}/.dotfiles/vim_runtime
 
-echo 'set runtimepath+=~/.vim_runtime
+cd ${HOME}/.dotfiles/vim_runtime
 
-source ~/.vim_runtime/vimrcs/basic.vim
-source ~/.vim_runtime/vimrcs/filetypes.vim
-source ~/.vim_runtime/vimrcs/plugins_config.vim
-source ~/.vim_runtime/vimrcs/extended.vim
+echo $VIMDIR
+
+echo "set runtimepath+=${VIMDIR}
+
+source ${VIMDIR}/vimrcs/basic.vim
+source ${VIMDIR}/vimrcs/filetypes.vim
+source ${VIMDIR}/vimrcs/plugins_config.vim
+source ${VIMDIR}/vimrcs/extended.vim
 
 try
-source ~/.vim_runtime/my_configs.vim
+source ${VIMDIR}/my_configs.vim
 catch
-endtry' > ~/.vimrc
+endtry" > ~/.vimrc
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
