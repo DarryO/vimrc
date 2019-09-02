310e07c7e51a5b3aab2bb653c7f0cd065560e429
Li Hao <lihao@fabu.ai>
2019-09-02 12:56:08 +0800

    Update

--- my_plugins/vim-template/templates/=template=.py
+++ my_plugins/vim-template/templates/=template=.py
@@ -10,24 +10,3 @@
 """
 %HERE%
 """
-
-import os
-import sys
-import argparse
-import pprint
-import logging
-
-PP = pprint.PrettyPrinter().pprint
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--debug", action="store_true", default=False, help="whether to debug")
-parser.add_argument("--output_dir", type=str, default='/tmp', help="")
-parser.add_argument("--num", type=float, default=0.7, help="")
-
-
-def main():
-    args = parser.parse_args()
-
-
-if __name__ == "__main__":
-    main()
